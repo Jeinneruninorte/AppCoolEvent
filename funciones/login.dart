4c20219d14254206b951716907cfc02f6ad29aed
Jeinneruninorte <jeinnerb@uninorte.edu.co>
2021-11-30 19:47:32 -0500

    codigo avance

---
+++ funciones/login.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+
+class Iniciosesion extends StatelessWidget {
+  const Iniciosesion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Inicio de sesión"),
+        backgroundColor: Colors.lightBlue,
+        foregroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              margin: const EdgeInsets.all(30.0),
+              child: const Text(
+                'COOL EVENT una app más allá de eventos',
+                style: TextStyle(
+                  color: Color.fromRGBO(46, 116, 154, 15),
+                  fontFamily: 'RobotoMono',
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+          MyForm(),
+        ],
+      ),
+      //MyForm(),
+    );
+  }
+}
+
+/*Column(
+        children: [
+          Center(
+            child: Container(
+              margin: const EdgeInsets.all(30.0),
+              child: const Text(
+                'COOL EVENT una app más allá de eventos',
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 45, 45, 15),
+                  fontFamily: 'RobotoMono',
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      
+    );
+  }
+}*/
+class MyForm extends StatefulWidget {
+  @override
+  LoginFormState createState() {
+    return LoginFormState();
+  }
+}
+
+class LoginFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _usuario(),
+          _password(),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text('Validando la información .  .  .')));
+                }
+              },
+              child: const Text('Login'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _usuario() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        icon: Icon(Icons.email),
+        hintText: 'Inserte su email',
+        labelText: 'Email',
+      ),
+      validator: (value) => _validaciones(value!),
+    );
+  }
+
+  Widget _password() {
+    return TextFormField(
+      decoration: const InputDecoration(
+        icon: Icon(Icons.password),
+        hintText: 'Inserte su contraseña',
+        labelText: 'Contraseña',
+      ),
+      validator: (value) => _validaciones(value!),
+    );
+  }
+
+  String? _validaciones(String value) {
+    if (value.isEmpty) {
+      return 'Ingrese algún texto';
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'ui/pages/login.dart';
+import 'ui/pages/registro.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cool Event',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'COOL EVENT'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+        titleTextStyle: const TextStyle(
+          fontFamily: 'Nova-Square',
+          fontSize: 40,
+        ),
+      ),
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ElevatedButton(
+                child: const Text('Registrarse'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegistroForm()));
+                }),
+            ElevatedButton(
+                child: const Text('Iniciar Sesión'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Iniciosesion()));
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
